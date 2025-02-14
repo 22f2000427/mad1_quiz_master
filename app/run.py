b3e801847d38c73ad988c58209f1d7a92c98ee06
22f2000427 <153759064+22f2000427@users.noreply.github.com>
2025-02-15 00:42:16 +0530

    Update run.py

--- app/run.py
+++ app/run.py
@@ -1,58 +1,26 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, request , jsonify , render_template , redirect , url_for , session
 from flask_sqlalchemy import SQLAlchemy
-from database import db, User, Subject, Chapter, Quiz, Question, Score
+from flask_login import LoginManager , login_user , login_required , current_user , logout_user
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///quiz_master.db'
-app.config['SECRET_KEY'] = 'your_secret_key'
+from models.models import Users, db
+from routes.login import login_bp
+
+app = Flask(__name__ , template_folder='templates')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///quiz.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.register_blueprint(login_bp)
+
 db.init_app(app)
-
-# Create database tables
 with app.app_context():
     db.create_all()
+    print("Tables created successfully")
 
-# Routes
 @app.route('/')
-def home():
-    return render_template('login.html')
+def main():
+    return render_template('main.html')
+import os
+print(f"Database path: {os.path.abspath('quiz.db')}")
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        user = User.query.filter_by(username=username, password=password).first()
-        if user:
-            if user.is_admin:
-                return redirect(url_for('admin_dashboard'))
-            else:
-                return redirect(url_for('user_dashboard'))
-        else:
-            flash('Invalid credentials!', 'error')
-    return render_template('login.html')
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        full_name = request.form['full_name']
-        qualification = request.form['qualification']
-        dob = request.form['dob']
-        new_user = User(username=username, password=password, full_name=full_name, qualification=qualification, dob=dob)
-        db.session.add(new_user)
-        db.session.commit()
-        flash('Registration successful!', 'success')
-        return redirect(url_for('login'))
-    return render_template('register.html')
-
-@app.route('/admin/dashboard')
-def admin_dashboard():
-    return render_template('admin_dashboard.html')
-
-@app.route('/user/dashboard')
-def user_dashboard():
-    return render_template('user_dashboard.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
